94ac4eb7f7bc4f1251768264755e00b7c6a92e44
Matt J Williams <spammable@nomovingparts.net>
2011-08-20 01:20:09 +0100

    first files

--- minctweet.py
+++ minctweet.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+#
+# Copyright 2011 Matthew J Williams
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 """
 A simple program that will monitor a Minecraft server and tweet interesting
@@ -12,9 +26,12 @@ import os.path
 import subprocess
 import time
 
+import tweepy
+
 class FileMonitor(object):
     """
-    Only lines added AFTER monitoring was initiated will be read.
+    A monitor that reads additions to a text file. Lines existing before 
+    monitoring was initiated are ignored.
     """
     
     def __init__( self, fpath ):
@@ -38,7 +55,8 @@ class FileMonitor(object):
     
     def nextline( self ):
         """
-        Newline symbol is discarded.
+        Return the next line in the file. The newline symbol is discarded. 
+        This method will block until a new line is added to the file.
         """
         self.tailproc.poll()
         if self.tailproc.returncode is not None:
@@ -63,28 +81,50 @@ def main():
     
     if not os.path.exists( config_fpath ):
         print "%s: file does not exist" % (config_fpath)
+        exit()
     
     if not os.path.isfile( config_fpath ):
         print "%s: is not a file" % (config_fpath)
+        exit()
     
     config_dict = {}
     fin = open( config_fpath, 'r' )
     rdr = csv.reader( fin, delimiter='=' )
     for row in rdr:
-        print row
         (field,val) = row
         config_dict[field] = val
     fin.close()
     
     #
+    # Auth twitter
+    consumer_key = config_dict['consumer_key']
+    consumer_secret = config_dict['consumer_secret']
+    access_key = config_dict['access_key']
+    access_secret = config_dict['access_secret']
+    
+    #~ to do: connect to twitter
+    #auth = tweepy.OAuthHandler( self.__consumer_key, self.__consumer_secret )
+    #auth.set_access_token( self.__access_key, self.__access_secret )
+    #api = tweepy.API( auth )
+    
+    #
     # Hook into log file
     mclog_fpath = config_dict['log']
+    
+    if not os.path.exists( mclog_fpath ):
+        print "%s: file does not exist" % (mclog_fpath)
+        exit()
+    
+    if not os.path.isfile( mclog_fpath ):
+        print "%s: is not a file" % (mclog_fpath)
+        exit()
+    
     logmon = FileMonitor( mclog_fpath )
 
     while True:
         ln = logmon.nextline()
         print ln
-        #Log monitoring quit unexpectedly
+        #~ to do: handling of the line of log text...
         
 if __name__ == '__main__':
     main()
